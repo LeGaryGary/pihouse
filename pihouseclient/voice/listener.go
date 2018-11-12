5972bc2ec66d9b3e6ebebdde19fd1565115126ed
Jordan Kelwick <jordankelwick@gmail.com>
2018-11-12 23:00:18 +0000

    WIP

--- pihouseclient/voice/listener.go
+++ pihouseclient/voice/listener.go
@@ -141,52 +141,56 @@ func listen(p porcupine.Porcupine, audio io.Reader, shutdownChan <-chan os.Signa
 
 			if word != "" {
 				log.Printf("detected word: \"%s\"", word)
+				apiStreamStopChan := make(chan bool)
 				go func() {
 					// Pipe stdin to the API.
 					buf := make([]byte, 1024)
 					for {
-						n, err := os.Stdin.Read(buf)
-						if n > 0 {
-							if err := stream.Send(&speechpb.StreamingRecognizeRequest{
-								StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
-									AudioContent: buf[:n],
-								},
-							}); err != nil {
-								log.Printf("Could not send audio: %v", err)
-							}
-						}
-						if err == io.EOF {
-							// Nothing else to pipe, close the stream.
-							if err := stream.CloseSend(); err != nil {
-								log.Fatalf("Could not close stream: %v", err)
-							}
+						select {
+						case <-apiStreamStopChan:
 							return
-						}
-						if err != nil {
-							log.Printf("Could not read from stdin: %v", err)
-							continue
+						default:
+							n, err := os.Stdin.Read(buf)
+							if n > 0 {
+								if err := stream.Send(&speechpb.StreamingRecognizeRequest{
+									StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
+										AudioContent: buf[:n],
+									},
+								}); err != nil {
+									log.Printf("Could not send audio: %v", err)
+								}
+							}
+							if err == io.EOF {
+								// Nothing else to pipe, close the stream.
+								if err := stream.CloseSend(); err != nil {
+									log.Fatalf("Could not close stream: %v", err)
+								}
+								return
+							}
+							if err != nil {
+								log.Printf("Could not read from stdin: %v", err)
+								continue
+							}
 						}
 					}
 				}()
 
-				for {
-					resp, err := stream.Recv()
-					if err == io.EOF {
-						break
+				resp, err := stream.Recv()
+				if err == io.EOF {
+					continue
+				}
+				if err != nil {
+					log.Fatalf("Cannot stream results: %v", err)
+				}
+				if err := resp.Error; err != nil {
+					// Workaround while the API doesn't give a more informative error.
+					if err.Code == 3 || err.Code == 11 {
+						log.Print("WARNING: Speech recognition request exceeded limit of 60 seconds.")
 					}
-					if err != nil {
-						log.Fatalf("Cannot stream results: %v", err)
-					}
-					if err := resp.Error; err != nil {
-						// Workaround while the API doesn't give a more informative error.
-						if err.Code == 3 || err.Code == 11 {
-							log.Print("WARNING: Speech recognition request exceeded limit of 60 seconds.")
-						}
-						log.Fatalf("Could not recognize: %v", err)
-					}
-					for _, result := range resp.Results {
-						fmt.Printf("Result: %+v\n", result)
-					}
+					log.Fatalf("Could not recognize: %v", err)
+				}
+				for _, result := range resp.Results {
+					fmt.Printf("Result: %+v\n", result)
 				}
 				// [END speech_transcribe_streaming_mic]
 			}
