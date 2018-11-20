c54603b1e5f00f51bb6d27142f64b21dee412cde
Jordan Kelwick <jordankelwick@gmail.com>
2018-11-20 20:18:51 +0000

    WIP

--- pihouseclient/api/ai.go
+++ pihouseclient/api/ai.go
@@ -13,7 +13,10 @@ import (
 func PostAIIntent(outcomes []wit.Outcome) {
 	b := new(bytes.Buffer)
 	json.NewEncoder(b).Encode(outcomes)
-	hostname := os.Hostname
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
 	res, err := http.Post("http://"+APIAddress+"/v1/api/ai/outcomes/"+hostname, "application/json; charset=utf-8", b)
 	if err != nil {
 		panic(err)
