e4c9459f4fb755b551df9fcc66ef684a98355521
Jordan Kelwick <jordankelwick@gmail.com>
2018-12-01 14:51:25 +0000

    got da test working

--- pihouseserver/control/clientController_test.go
+++ pihouseserver/control/clientController_test.go
@@ -7,29 +7,80 @@ import (
 	"github.com/Jordank321/pihouse/data"
 )
 
+import . "github.com/ahmetb/go-linq"
+
 type TestAIRepository struct {
+	mappings []data.ActionMaping
 }
 
 func (testRepo *TestAIRepository) NewWitAIOutcome(request *data.AIRequest) {
 
 }
 func (testRepo *TestAIRepository) FindActions(intentValue string) []data.ActionMaping {
+	return testRepo.mappings
+}
 
+type TestClient struct {
+	applicableActions []data.Action
+	actionsSent       []data.Action
+}
+
+func (client *TestClient) GetApplicableActions() []data.Action {
+	return client.applicableActions
+}
+
+func (client *TestClient) SendAction(action data.Action) {
+	client.actionsSent = append(client.actionsSent, action)
+}
+
+func (client *TestClient) SetAsClosed() {
+}
+
+func (client *TestClient) String() string {
+	return "This is a test!"
 }
 
 func TestWebSocketClientController_ProcessRequest(t *testing.T) {
 	type args struct {
 		request *data.AIRequest
 	}
+	type result struct {
+		sentActions []data.Action
+	}
+
+	lightsClent := &TestClient{
+		applicableActions: []data.Action{
+			data.LivingRoomLightsOn,
+		},
+	}
+	heatingClient := &TestClient{
+		applicableActions: []data.Action{
+			data.HeatingOn,
+		},
+	}
+
 	tests := []struct {
 		name       string
+		client     *TestClient
 		controller *WebSocketClientController
 		args       args
+		result     result
 	}{
 		{
 			"AI request for living room lights on with single mapping",
+			lightsClent,
 			&WebSocketClientController{
-				aiRepository: &TestAIRepository{},
+				aiRepository: &TestAIRepository{
+					mappings: []data.ActionMaping{
+						data.ActionMaping{
+							Action:      data.LivingRoomLightsOn,
+							IntentValue: "intent:lights_living_room,on_off:on",
+						},
+					},
+				},
+				clients: &[]Client{
+					lightsClent,
+				},
 			},
 			args{
 				request: &data.AIRequest{
@@ -43,11 +94,64 @@ func TestWebSocketClientController_ProcessRequest(t *testing.T) {
 					},
 				},
 			},
+			result{
+				sentActions: []data.Action{
+					data.LivingRoomLightsOn,
+				},
+			},
+		},
+		{
+			"AI request for heating on with multiple mappings",
+			heatingClient,
+			&WebSocketClientController{
+				aiRepository: &TestAIRepository{
+					mappings: []data.ActionMaping{
+						data.ActionMaping{
+							Action:      data.HeatingOn,
+							IntentValue: "intent:heating,on_off:on",
+						},
+						data.ActionMaping{
+							Action:      data.HeatingOff,
+							IntentValue: "intent:heating,on_off:off",
+						},
+					},
+				},
+				clients: &[]Client{
+					heatingClient,
+				},
+			},
+			args{
+				request: &data.AIRequest{
+					Intents: []data.Intent{
+						data.Intent{
+							Value: "intent:heating",
+						},
+						data.Intent{
+							Value: "on_off:on",
+						},
+					},
+				},
+			},
+			result{
+				sentActions: []data.Action{
+					data.HeatingOn,
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.controller.ProcessRequest(tt.args.request)
+			missingSentActions := []data.Action{}
+			From(tt.result.sentActions).Where(func(sa interface{}) bool {
+				return !From(tt.client.actionsSent).Contains(sa.(data.Action))
+			}).ToSlice(&missingSentActions)
+			for _, missingAction := range missingSentActions {
+				t.Errorf("Expected sent actions to contain %s", missingAction)
+			}
+			if len(tt.result.sentActions) != len(tt.client.actionsSent) {
+				t.Error("Mismatch in sent actions!")
+			}
 		})
 	}
 }
