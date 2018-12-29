6cd848d143245b0c236cc27a4aa8fe2e2d13233d
Jordan Kelwick <jordankelwick@gmail.com>
2018-12-29 13:48:01 +0000

    WIP

--- pihouseclient/temphumidreader/main.go
+++ pihouseclient/temphumidreader/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	dht "github.com/d2r2/go-dht"
 )
 
 func main() {
 	temperature, humidity, _, err := dht.ReadDHTxxWithRetry(dht.DHT11, 2, true, 10)
-	print(temperature.String() + " " + humidity.String())
+	fmt.Printf("%f %f", temperature, humidity)
 }
